94bf544977a48d3e642742ebb124bb6375994f2c
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2022-05-03 14:07:14 +0200

    remove unneeded if

--- quarry.lua
+++ quarry.lua
@@ -1,10 +1,7 @@
 
 local quarry_def = minetest.registered_nodes["technic:quarry"]
 assert(quarry_def)
-if quarry_def ~= nil then
-	print("[monitoring] wrapping quarry.technic_run")
 
-	quarry_def.technic_run = monitoring
-	.counter("technic_quarry_dig_count", "number of technic quarry digs")
+quarry_def.technic_run =
+	monitoring.counter("technic_quarry_dig_count", "number of technic quarry digs")
 	.wrap(quarry_def.technic_run)
-end
