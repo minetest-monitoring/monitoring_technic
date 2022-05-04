ff6af94ef04cd1b4c0a6e6949baf6f4471b4abd1
Buckaroo Banzai <39065740+BuckarooBanzay@users.noreply.github.com>
2022-05-04 15:00:37 +0200

    add switching station metrics (#1)
    * add switching station metrics
    
    * remove unneeded if
    
    * fix wrong import
    
    Co-authored-by: BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>

--- quarry.lua
+++ quarry.lua
@@ -1,9 +1,7 @@
 
-local quarry_node = minetest.registered_nodes["technic:quarry"]
-if quarry_node ~= nil then
-	print("[monitoring] wrapping quarry.technic_run")
+local quarry_def = minetest.registered_nodes["technic:quarry"]
+assert(quarry_def)
 
-	quarry_node.technic_run = monitoring
-	.counter("technic_quarry_dig_count", "number of technic quarry digs")
-	.wrap(quarry_node.technic_run)
-end
+quarry_def.technic_run =
+	monitoring.counter("technic_quarry_dig_count", "number of technic quarry digs")
+	.wrap(quarry_def.technic_run)
